db3406510f6853d9f9b1b887c982a5935fcfe33c
Jethro Glaudin <jethroglaudin.student@careerdevs.com>
2019-03-30 03:51:41 +0000

    updated toDoList.js refactored version

--- toDoList.js
+++ toDoList.js
@@ -1,80 +1,103 @@
+//refactored version
+
 var todoList = {
-    todos: [],
-    displayTodos: function() {
-        if (this.todos.length === 0) {
-            console.log('Your todo list is empty!');
-        }
-        else {
-            console.log('My Todos:');
-            for (var i = 0; i < this.todos.length; i++) {
-                if (this.todos[i].completed === true) {
-                    console.log('(x)', this.todos[i].todoText);
-                }
-                else {
-                    console.log('( )', this.todos[i].todoText);
-                }
-            }
-        }
-    },
-    addTodo: function(todoText) {
-        this.todos.push({
-            todoText: todoText,
-            completed: false
-        });
-        this.displayTodos();
-    },
-    changeTodo: function(position, todoText) {
-        this.todos[position].todoText = todoText;
-        this.displayTodos();
-    },
-    deleteTodo: function(position) {
-        this.todos.splice(position, 1);
-        this.displayTodos();
-    },
+  todos: [],
+  displayTodos: function() {
 
-    toggleCompleted: function(position) {
-        var todo = this.todos[position];
-        todo.completed = !todo.completed;
-        this.displayTodos();
-    },
-    toggleAll: function() {
-        var totalTodos = this.todos.length;
-        var completedTodos = 0;
-
-        //Get # of completed Todos
-        for (var i = 0; i < totalTodos; i++) {
-            if (this.todos[i].completed === true) {
-                completedTodos++;
-            }
+    if (this.todos.length === 0) {
+      console.log('Your todo list is empty!');
+    } else {
+      console.log('My Todos:');
+      for (var i = 0; i < this.todos.length; i++) {
+        if (this.todos[i].completed === true) {
+          console.log('(x)', this.todos[i].todoText);
+        } else {
+          console.log('( )', this.todos[i].todoText);
         }
-        // Case 1: If everything's true, make everything false
-        if (completedTodos === totalTodos) {
-            for (var i = 0; i < totalTodos; i++) {
-                this.todos[i].completed = false;
-            }
-            // Case 2: Otherwise, make everything true.
-        }
-        else {
-            for (var i = 0; i < totalTodos; i++) {
-                this.todos[i].completed = true;
-            }
-
-        }
-        this.displayTodos();
-        // Make everything false
+      }
     }
+  },
+  addTodo: function(todoText) {
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
+    this.displayTodos();
+  },
+  changeTodo: function(position, todoText) {
+    this.todos[position].todoText = todoText;
+    this.displayTodos();
+  },
+  deleteTodo: function(position) {
+    this.todos.splice(position, 1);
+    this.displayTodos();
+  },
+
+  toggleCompleted: function(position) {
+    var todo = this.todos[position];
+    todo.completed = !todo.completed;
+    this.displayTodos();
+  },
+  toggleAll: function(){
+    var totalTodos = this.todos.length;
+    var completedTodos = 0;
+
+
+    for (var i=0; i < totalTodos; i++){
+      if (this.todos[i].completed === true) {
+        completedTodos++;
+      }
+    }
+
+    if (completedTodos === totalTodos) {
+      for (var i = 0; i <totalTodos; i++){
+        this.todos[i].completed = false;
+      }
+
+    } else {
+      for (var i = 0; i <totalTodos; i++){
+        this.todos[i].completed = true;
+      }
+
+    }
+    this.displayTodos();
+
+  }
 };
 
-// 1. We want to get access to the display todos button
-var displayTodosButton = document.getElementById('displayTodosButton');
-var toggleAllButton = document.getElementById('toggleAllButton');
 
-// 2. we Want to run displayTodos method, when someone clicks
-
-displayTodosButton.addEventListener('click', function() {
+var handlers = {
+  // we want the methods on this object to handle different clicks
+  displayTodos: function() {
     todoList.displayTodos();
-});
+},
 
-toggleAllButton.addEventListener('click', function() {
-    todoList.toggleAll();
-});
+  addTodo: function(){
+    document.getElementById('addTodoTextInput');
+    todoList.addTodo(addTodoTextInput.value);
+    addTodoTextInput.value = '';
+  },
+
+  changeTodo: function (){
+    var changeTodoPositionInput = document.getElementById('changeTodoPositionInput');
+    var changeTodoTextInput = document.getElementById('changeTodoTextInput');
+    todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
+    changeTodoPositionInput.value = '';
+    changeTodoTextInput.value = '';
+  },
+
+  deleteTodo: function (){
+    var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
+    todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
+    deleteTodoPositionInput.value = '';
+  },
+
+toggleCompleted: function(){
+  var toggleCompletedPositionInput = document.getElementById('toggleCompletedPositionInput');
+  todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
+  toggleCompletedPositionInput.value = '';
+},
+toggleAll: function() {
+  todoList.toggleAll();
+}
+};
